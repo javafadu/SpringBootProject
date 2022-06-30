0cd068171612dd6058e4a89dbb2e9fa77e1060ea
javafadu <javafadu@gmail.com>
2022-06-30 14:17:28 +0300

    30jun2022-1420

--- src/main/java/com/tpe/domain/Book.java
+++ src/main/java/com/tpe/domain/Book.java
@@ -18,32 +18,32 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Book {
 
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@JsonProperty("bookName")
-	private String name;
-	
-	@JsonIgnore
-	@ManyToOne
-	@JoinColumn(name="student_id")
-	private Student student;
-	
-	
-	public Long getId() {
-		return id;
-	}
 
-	public String getName() {
-		return name;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Student getStudent() {
-		return student;
-	}
+    @JsonProperty("bookName")
+    private String name;
 
-	
-	
+    @JsonIgnore  // sonsuz loop a girme durumu oluyor bu olmazsa.
+                    // Book un icinden student i tekrar getirmesini kiriyoruz. Recursive olmuyor
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+
 }
